e4ea3ca02ea595f0c14a45c2555c005de1e0d88a
Cam0411 <89090731+Cam0411@users.noreply.github.com>
2022-01-17 12:02:15 +0700

    Add files via upload

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ const temp = document.querySelector(".weather__app .temp")
 const body = document.querySelector("body")
 const weather_main = document.querySelector(".weather__app .weather")
 const hightLowWeather = document.querySelector(".weather__app .min__max-temp")
+const icon = document.querySelector(".weather__app .icon")
 const presure = document.querySelector(".weather__app .presure")
 const humidity = document.querySelector(".weather__app .humidity")
 const searchbox = document.querySelector('.search');
@@ -40,31 +41,48 @@ const api = {
       humidity.innerText = ""
   }
   function displayResults(weather){
+   
       city.innerText = `${weather.name}, ${weather.sys.country}`
       const today = new Date()
       date.innerText = buildDate(today)
       temp.innerText = `${Math.round(weather.main.temp)}°c`
       weather_main.innerText = weather.weather[0].main
-    
       hightLowWeather.innerText = `${Math.round(weather.main.temp_min)}°c  /  ${Math.round(weather.main.temp_max)}°c`
       presure.innerHTML = `<i class="fas fa-tint"></i> ${Math.round(weather.main.pressure)}`
       humidity.innerHTML = `<i class="fas fa-tint-slash"></i> ${Math.round(weather.main.humidity)}`
-          if ( weather_main.innerText == "Clouds"){
-        document.body.style.background = "linear-gradient( rgba(0, 0, 0, 0.5) 100%, rgba(0, 0, 0, 0.5)100%),url('https://www.desktopbackground.org/download/1024x600/2010/06/11/31550_laptop-1366x768-vector-wallpapers-desktop-backgrounds-hd_1366x768_h.jpg') no-repeat  fixed,  center  "
+      changeBackground()
+    } 
+  
+function changeBackground(){
+   
+    if ( weather_main.innerText == "Clouds"){
+        document.body.style.background = "linear-gradient( rgba(0, 0, 0, 0.5) 100%, rgba(0, 0, 0, 0.5)100%),url('https://i.pinimg.com/originals/e0/a3/b7/e0a3b7122280d22b83b22155e9436ade.jpg') no-repeat  fixed,  center  "
         document.body.style.backgroundSize = "cover";
       }else if (weather_main.innerText == "Clear"){
         document.body.style.background = "linear-gradient( rgba(0, 0, 0, 0.5) 100%, rgba(0, 0, 0, 0.5)100%),url('https://swall.teahub.io/photos/small/13-131905_firewatch-wallpapers-olly-moss.png') no-repeat  fixed,  center  "
         document.body.style.backgroundSize = "cover"
       }
-   
-    } 
-  
-function changeBackground(){
-   
-
-    
+      else if (weather_main.innerText == "Rain"){
+        document.body.style.background = "linear-gradient( rgba(0, 0, 0, 0.5) 100%, rgba(0, 0, 0, 0.5)100%),url('https://i.pinimg.com/originals/3f/49/74/3f4974bcf8b4b2a71663b825c2aea282.png') no-repeat  fixed,  center  "
+        document.body.style.backgroundSize = "cover"
+      }
+      else if (weather_main.innerText == "Snow"){
+        document.body.style.background = "linear-gradient( rgba(0, 0, 0, 0.5) 100%, rgba(0, 0, 0, 0.5)100%),url('https://i.pinimg.com/originals/e3/13/a0/e313a08e5b0455b1d2b5f345b2cdb97f.jpg') no-repeat  fixed,  center  "
+        document.body.style.backgroundSize = "cover"
+      }
+      else if (weather_main.innerText == "Thunderstorm"){
+        document.body.style.background = "linear-gradient( rgba(0, 0, 0, 0.5) 100%, rgba(0, 0, 0, 0.5)100%),url('https://c4.wallpaperflare.com/wallpaper/344/980/911/mozilla-illustration-mozilla-firefox-vector-art-wallpaper-preview.jpg') no-repeat  fixed,  center  "
+        document.body.style.backgroundSize = "cover"
+      }
+      else if (weather_main.innerText == "Drizzle"){
+        document.body.style.background = "linear-gradient( rgba(0, 0, 0, 0.5) 100%, rgba(0, 0, 0, 0.5)100%),url('https://i.pinimg.com/736x/fa/9b/40/fa9b4044348846f50fa1e10a5d80a8b5--illustration-vector-logo-branding.jpg') no-repeat  fixed,  center  "
+        document.body.style.backgroundSize = "cover"
+      }else {
+        document.body.style.background = "linear-gradient( rgba(0, 0, 0, 0.5) 100%, rgba(0, 0, 0, 0.5)100%),url('https://images.pond5.com/egypt-pyramids-and-nile-river-illustration-149182068_iconl_wide_nowm.jpeg') no-repeat  fixed,  center  "
+        document.body.style.backgroundSize = "cover"
+      }
 }
-changeBackground()
+
   function buildDate(d){
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
